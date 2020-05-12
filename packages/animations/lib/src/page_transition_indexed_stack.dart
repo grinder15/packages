1a35579e141a772790706b615530d2e478b372f1
grinder15 <arenasjerome50@gmail.com>
2020-05-12 17:08:59 +0800

    [animations] PageTransitionIndexedStack Added
    A widget equivalent to [IndexedStack] but it animates it's widgets when
    [index] parameter is changed, also preserves child state even out of
    the view and can be configured to lazily attach it's widget into the
    subtree. It can be used to implement lateral navigation such as
    BottomNavigationBar, TabViews, and Drawers.

---
+++ packages/animations/lib/src/page_transition_indexed_stack.dart
@@ -0,0 +1,289 @@
+// Copyright 2019 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+typedef PageTransitionIndexedStackBuilder = Widget Function(Widget child,
+    Animation<double> animation, Animation<double> secondaryAnimation);
+
+///
+// TODO(grinder): much better if don't build transition at the creation of
+//  _ChildEntry, instead just build the transition at build() method to reflect
+//  any changes in transitionBuilder, duration, and children parameters.
+class PageTransitionIndexedStack extends StatefulWidget {
+  ///
+  const PageTransitionIndexedStack({
+    Key key,
+    @required this.index,
+    @required this.children,
+    @required this.transitionBuilder,
+    this.duration = const Duration(milliseconds: 300),
+    this.lazy = false,
+    this.reverse = false,
+  })  : assert(index != null),
+        assert(children != null),
+        assert(transitionBuilder != null),
+        assert(duration != null),
+        super(key: key);
+
+  ///
+  final int index;
+
+  ///
+  // TODO(grinder15): If children changes, what will happen?
+  // not sure, I think if we check all widgets against the previous and re-wrap
+  // them with transitionBuilder
+  // possible checks: by children.length and [Widget.canUpdate]
+  final List<Widget> children;
+
+  ///
+  // TODO(grinder15): If duration changes, what will happen?
+  // possible: flush all _ChildEntry and recreate thus destroying the state of
+  // widgets? or re wrap again?
+  final Duration duration;
+
+  ///
+  // TODO(grinder15): If transitionBuilder changes, what will happen?
+  // current scenario: it will reset all children, re-wraps all children with new
+  // transitionBuilder and animate-in the child with the supplied index.
+  final PageTransitionIndexedStackBuilder transitionBuilder;
+
+  ///
+  // TODO(grinder15): If lazy changes, what will happen?
+  // current scenario: the widget will not allow changes in lazy thus assertion
+  // check will trigger.
+  final bool lazy;
+
+  ///
+  final bool reverse;
+
+  @override
+  _PageTransitionIndexedStackState createState() =>
+      _PageTransitionIndexedStackState();
+}
+
+class _PageTransitionIndexedStackState extends State<PageTransitionIndexedStack>
+    with TickerProviderStateMixin {
+  List<_ChildEntry> _children;
+  final Set<int> _setIndexes = <int>{};
+  final Set<int> _activeIndexes = <int>{};
+
+  @override
+  void initState() {
+    super.initState();
+    _setNewChildrenList();
+    assert(_children.isNotEmpty);
+    // initial complete the animation.
+    if (widget.index != null) {
+      if (widget.lazy) {
+        _addIndexToSet(widget.index);
+      }
+      _activeIndexes.add(widget.index);
+      _children[widget.index].primaryController.value = 1.0;
+    }
+  }
+
+  void _addIndexToSet(int index) {
+    _setIndexes.add(index);
+  }
+
+  void _setNewChildrenList() {
+    _children = widget.children
+        .asMap()
+        .map(
+          (int index, Widget child) {
+            // create animation controller
+            final AnimationController _primaryController = AnimationController(
+              vsync: this,
+              duration: widget.duration,
+            );
+
+            final AnimationController _secondaryController =
+                AnimationController(
+              vsync: this,
+              duration: widget.duration,
+            );
+
+            _primaryController.addStatusListener((AnimationStatus status) {
+              if (status == AnimationStatus.dismissed) {
+                setState(() {
+                  _activeIndexes.remove(index);
+                });
+              }
+            });
+
+            _secondaryController.addStatusListener((AnimationStatus status) {
+              if (status == AnimationStatus.completed) {
+                setState(() {
+                  _activeIndexes.remove(index);
+                });
+              }
+            });
+
+            final Widget _transition = widget.transitionBuilder(
+              child,
+              _primaryController,
+              _secondaryController,
+            );
+
+            assert(
+              _transition != null,
+              'AnimatedIndexedStack.transitionBuilder must not return null.',
+            );
+
+            return MapEntry<int, _ChildEntry>(
+              index,
+              _ChildEntry(
+                index: index,
+                primaryController: _primaryController,
+                secondaryController: _secondaryController,
+                widgetChild: child,
+                transition: _transition,
+              ),
+            );
+          },
+        )
+        .values
+        .toList();
+  }
+
+  void _animateChild(_ChildEntry _childEntry,
+      {bool exit = false, bool reverse = false}) {
+    // animate the child
+    if (exit) {
+      // play exit animation
+      _childEntry.primaryController.value = 1.0;
+      _childEntry.secondaryController.forward(from: 0.0);
+    } else {
+      // play enter animation
+      _childEntry.secondaryController.value = 0.0;
+      _childEntry.primaryController.forward(from: 0.0);
+    }
+  }
+
+  @override
+  void didUpdateWidget(PageTransitionIndexedStack oldWidget) {
+    assert(widget.lazy == oldWidget.lazy,
+        "You can't change lazy parameter in rebuild.");
+    super.didUpdateWidget(oldWidget);
+
+    // recreate all children entries whenever the list length is different or
+    // duration is different
+    if (widget.children.length != _children.length ||
+        widget.duration != oldWidget.duration) {
+      // if lazy and there is a change, reset all indexes.
+      if (widget.lazy) {
+        // TODO(grinder15): what is the use of _setIndexes?
+        _setIndexes.clear();
+      }
+      _activeIndexes.clear();
+
+      // dispose children first.
+      _disposeChildren();
+      // update all children entries
+      _setNewChildrenList();
+      // play animation
+      if (widget.index != null) {
+        _activeIndexes.add(widget.index);
+        _animateChild(_children[widget.index]);
+      }
+      // Note: we don't need to setState here cuz we will not perform exit animation
+      // for the previous active index. save build cost.
+      return;
+    }
+
+    assert(widget.children.length == oldWidget.children.length,
+        "You changed the children but isn't refreshed");
+
+    // update children.
+    // TODO(grinder15): Do we need to update children?
+    //_updateChildren(widget.children);
+
+    if (widget.index != oldWidget.index) {
+      // animate!
+      // if the new index is null don't animate in!
+      if (widget.index != null) {
+        // if lazy, cache that index
+        if (widget.lazy) {
+          _addIndexToSet(widget.index);
+        }
+        _activeIndexes.add(widget.index);
+        _animateChild(_children[widget.index]);
+      }
+      if (oldWidget.index != null) {
+        _activeIndexes.add(widget.index);
+        _animateChild(_children[oldWidget.index], exit: true);
+      }
+    }
+  }
+
+  void _disposeChildren() {
+    for (final _ChildEntry _child in _children) {
+      _child.dispose();
+    }
+  }
+
+  /*void _updateChildren(List<Widget> newChildren) {
+    for (int i = 0; i < newChildren.length; i++) {
+      _children[i].widgetChild = newChildren[i];
+    }
+  }*/
+
+  @override
+  void dispose() {
+    _disposeChildren();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: _children.map(
+        (_ChildEntry child) {
+          if (widget.lazy) {
+            return Offstage(
+              offstage: !_activeIndexes.contains(child.index),
+              child: _setIndexes.contains(child.index)
+                  ? child.transition
+                  : Container(),
+            );
+          }
+          return Offstage(
+            offstage: !_activeIndexes.contains(child.index),
+            child: child.transition,
+          );
+        },
+      ).toList(),
+    );
+  }
+}
+
+class _ChildEntry {
+  _ChildEntry({
+    @required this.index,
+    @required this.primaryController,
+    @required this.secondaryController,
+    @required this.widgetChild,
+    @required this.transition,
+  })  : assert(index != null),
+        assert(primaryController != null),
+        assert(secondaryController != null),
+        assert(widgetChild != null),
+        assert(transition != null);
+
+  final int index;
+
+  final AnimationController primaryController;
+
+  final AnimationController secondaryController;
+
+  Widget transition;
+
+  Widget widgetChild;
+
+  void dispose() {
+    primaryController.dispose();
+    secondaryController.dispose();
+  }
+}
