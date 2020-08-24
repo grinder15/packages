17cb9772f484b3a90b953bb8c3a945855f632480
grinder15 <arenasjerome50@gmail.com>
2020-08-24 21:46:51 +0800

    [animations/personal] exporting page transition indexed stack.

--- packages/animations/lib/animations.dart
+++ packages/animations/lib/animations.dart
@@ -6,5 +6,6 @@ export 'src/fade_scale_transition.dart';
 export 'src/fade_through_transition.dart';
 export 'src/modal.dart';
 export 'src/open_container.dart';
+export 'src/page_transition_indexed_stack.dart';
 export 'src/page_transition_switcher.dart';
 export 'src/shared_axis_transition.dart';
